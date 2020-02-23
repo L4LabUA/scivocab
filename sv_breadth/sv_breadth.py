9c25fba5d2b05f0ffb5b097e361f4ad39b280e2e
jaerikson <jaerikson@gmail.com>
2020-02-23 13:23:05 -0700

    updated login window to have 'continue' button to exit login window and created page window for breadth task

--- sv_breadth/sv_breadth.py
+++ sv_breadth/sv_breadth.py
@@ -1,41 +1,70 @@
+# run app jar
+
 from appJar import gui
 
 # login window
 
-def press():
-    print("Breadth Task","User:", app.entry("User"), "Part:", app.entry("Participant"), app.getDatePicker("date"))
+def press(btn):
+    if btn == "Submit":
+        app.addLabel("submit", "Submitted")
+        print("Breadth Login", "User:", app.entry("User"), "Part:",
+              app.entry("Participant"), app.getDatePicker("date"))
+        app.addButton("Continue", app.stop)
 
-with gui("Breadth Login", "300x350", bg='orange', font={'size': 18}) as app:
-    app.label("Breadth Task Login", bg='blue', fg='orange')
-    app.entry("User", label=True, focus=True)
+with gui('Breadth Login', sticky='esw', stretch='column') as app:
+
+    app.setFont(14)
+    app.setBg("orange")
+
+    app.label("title", "Breadth Task Login", bg='blue', fg='orange', stretch='column', sticky='nesw')
+
+    app.entry("User", label=True)
     app.entry("Participant", label=True, secret=False)
     app.addDatePicker("date")
     app.setDatePickerRange("date", 2020)
     app.setDatePicker("date")
     app.getDatePicker("date")
     app.buttons(["Submit"], [press])
-    app.buttons(["Continue"], [app.stop])
 
-# breadth task
+    def press(btn):
+        print(btn)
 
-def press(btn):
-    print(btn)
+with gui('Breadth Task', sticky='esw', stretch='column') as app:
 
-app = gui("Breadth")
+    app.startPagedWindow("")
+    app.startPage("nesw")
+    app.label("Item 1", bg='blue', fg='orange', row=0, column=1, colspan=2)
+    app.label("tw goes here", row=1, column=1, colspan=2)
+    app.addImageButton("Item 1: A", press, "b001_fp.gif", row=2, column=1)
+    app.addImageButton("Item 1: B", press, "b001_fs.gif", row=2, column=2)
+    app.addImageButton("Item 1: C", press, "b001_fx.gif", row=3, column=1)
+    app.addImageButton("Item 1: D", press, "b001_tw.gif", row=3, column=2)
+    app.stopPage()
 
-app.setFont(10)
-app.setExpand("both")
-app.setSticky("ew")
+    app.startPage("nesw")
+    app.label("Item 2", bg='blue', fg='orange', row=0, column=1, colspan=2)
+    app.label("tw goes here", row=1, column=1, colspan=2)
+    app.addImageButton("Item 2: A", press, "b002_fp.gif", row=2, column=1)
+    app.addImageButton("Item 2: B", press, "b002_fs.gif", row=2, column=2)
+    app.addImageButton("Item 2: C", press, "b002_fx.gif", row=3, column=1)
+    app.addImageButton("Item 2: D", press, "b002_tw.gif", row=3, column=2)
+    app.stopPage()
 
-app.startLabelFrame("Breadth 1", 3, 2, 1)
-app.addImageButton("Breadth 1, A", press, "b001_fp.gif", 1, 1)
-app.addImageButton("Breadth 1, B", press, "b001_fs.gif", 1, 2)
-app.addImageButton("Breadth 1, C", press, "b001_fx.gif", 2, 1)
-app.addImageButton("Breadth 1, D", press, "b001_tw.gif", 2, 2)
-app.addButton("FIRST", press, 0, 1)
-app.addButton("PREV", press, 0, 2)
-app.addButton("NEXT", press, 3, 1)
-app.addButton("LAST", press, 3, 2)
-app.stopLabelFrame()
+    app.startPage("nesw")
+    app.label("Item 3", bg='blue', fg='orange', row=0, column=1, colspan=2)
+    app.label("tw goes here", row=1, column=1, colspan=2)
+    app.addImageButton("Item 3: A", press, "b001_fp.gif", row=2, column=1)
+    app.addImageButton("Item 3: B", press, "b001_fs.gif", row=2, column=2)
+    app.addImageButton("Item 3: C", press, "b001_fx.gif", row=3, column=1)
+    app.addImageButton("Item 4:, D", press, "b001_tw.gif", row=3, column=2)
+    app.stopPage()
 
-app.go()
+    app.startPage("nesw")
+    app.label("Item 4", bg='blue', fg='orange', row=0, column=1, colspan=2)
+    app.label("tw goes here", row=1, column=1, colspan=2)
+    app.addImageButton("Item 4: A", press, "b001_fp.gif", row=2, column=1)
+    app.addImageButton("Item 4: B", press, "b001_fs.gif", row=2, column=2)
+    app.addImageButton("Item 4: C", press, "b001_fx.gif", row=3, column=1)
+    app.addImageButton("Item 4: D", press, "b001_tw.gif", row=3, column=2)
+    app.stopPage()
+    app.stopPagedWindow()
