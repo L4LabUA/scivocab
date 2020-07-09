77ad991e108c21d2e10c3e2241aea32bcc2594f8
Sam <SammasC99@gmail.com>
2020-07-09 16:43:16 -0400

    toImage
    Added toimage, which takes a word and identifier, and returns the image filename

--- flask/translate.py
+++ flask/translate.py
@@ -2,16 +2,16 @@ import pandas
 from flask import WordConstruct
 
 
-
-def main():
+def main(args):
     allWords = dict()
-    graphinput = pandas.read_csv("static/scivocab/sv_bv1_input.csv")
+    graphinput = pandas.read_csv(args)
     for index, row in graphinput.iterrows():
         if row['target'] not in allWords:
             # The key doesn't exist. We need to define a new dictionary piece for it.
             allWords[row['target']] = WordConstruct.Word()
             allWords[row['target']].strand = row['strand']
             allWords[row['target']].id = row['breadth_id']
+            allWords[row['target']].target = row['target']
         # Now, we figure out which image we're looking at
         if row['img_type'] == 'tw':
             allWords[row['target']].tw = int(row['position'][1])
@@ -21,16 +21,20 @@ def main():
             allWords[row['target']].fx = int(row['position'][1])
         if row['img_type'] == 'fs':
             allWords[row['target']].fs = int(row['position'][1])
-    #for x in allWords:
-        #print(x + ' has its true word in position ' + str(allWords[x].tw) + " and is part of strand " + str(allWords[x].strand))
+    # or x in allWords:
+        # print(toimage(allWords[x], 'tw'))
+        # print(x + ' has its true word in position ' + str(allWords[x].tw) + " and is part of strand " + str(allWords[x].strand))
+
+
+# Translates a word to return
+def toimage(word, id):
+    if id == 'tw':
+        return "bv1_" + word.id + "_p" + str(word.tw) + "_tw.jpg"
+
+
+# main("static/scivocab/sv_bv1_input.csv")
 
 
 
 
 
-
-
-
-
-
-main()
