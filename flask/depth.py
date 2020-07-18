75df9eb68a0db366613c510b9b39cb388edcf171
Adarsh Pyarelal <adarsh@email.arizona.edu>
2020-07-18 11:08:52 -0700

    Adding temp MS Office files to .gitignore, implemented one-way dragging of images into ordering placeholder

---
+++ flask/depth.py
@@ -0,0 +1,63 @@
+from glob import glob
+from itertools import zip_longest
+from enum import Enum, auto
+from pathlib import Path
+from pandas import read_excel
+from random import shuffle
+import translate
+from itertools import chain
+from flask import (
+    Blueprint,
+    Flask,
+    render_template,
+    url_for,
+    request,
+    redirect,
+    jsonify,
+)
+
+
+# Flask blueprints help keep webapps modular.
+bp = Blueprint("depth", __name__)
+
+
+# Get all the words.
+WORDS = translate.construct_word_dict("static/scivocab/sv_bv1_input.csv")
+strands = [
+    [x for x in WORDS if WORDS[x].strand == n] for n in (40, 50, 62, 63)
+]
+
+for strand in strands:
+    shuffle(strand)
+
+randomized_list = list(chain(*strands))
+
+# Before I forget what this is- we use this in selectImage to put the images in
+# a random order.
+word_type_list = ["tw", "fp", "fx", "fs"]
+
+
+@bp.route("/")
+def main():
+    current_word = randomized_list[0]
+    return render_template("depth.html", current_word=current_word)
+
+
+@bp.route("/selectImage", methods=["GET", "POST"])
+def selectImage():
+    shuffle(word_type_list)
+    word_index = int(request.args.get("word_index", 0))
+    current_word = randomized_list[word_index]
+
+    # New response format for constructing data
+    response = {
+        f"p{n}_filename": translate.get_filename(
+            WORDS[current_word], word_type_list[n - 1]
+        )
+        for n in range(1, 5)
+    }
+    response["tw"] = current_word
+
+    # Append response to result.
+    # Dataframe.append - works for dictionaries.
+    return jsonify(response)
--- flask/run.py
+++ flask/run.py
@@ -1,10 +1,11 @@
 from breadth import bp as breadth_bp
+from depth import bp as depth_bp
 from flask import Flask
 
 def create_app():
     app = Flask(__name__)
     app.register_blueprint(breadth_bp, url_prefix="/breadth")
-    print(app.url_map)
+    app.register_blueprint(depth_bp, url_prefix="/depth")
     return app
 
 if __name__ == "__main__":
