ee29834886e29dfd99a58207514cb3f3a75c28e8
Siena Schoelen <schoelena@email.arizona.edu>
2020-09-24 15:49:44 -0700

    Landing page html and connection between login and landingpage

--- login.py
+++ login.py
@@ -1,14 +1,48 @@
-from flask import Blueprint
-from flask import render_template
+#original code is # out
 
+#from flask import Blueprint
+#from flask import render_template
+
+#bp = Blueprint("login", __name__)
+
+
+#@bp.route("/")
+#def main():
+#    return render_template("login.html")
+
+#@bp.route("/login",  methods=["GET", "POST"])
+#def login():
+#    return
+
+#the path: scivocab/login.py
+ 
+import functools
+ 
+from flask import (
+    Blueprint, flash, g, redirect, render_template, request, session, url_for
+)
+
+#this might be useful later when we want to store user names and passwords
+#from flaskr.db import get_db
+ 
 bp = Blueprint("login", __name__)
-
-@bp.route("/")
+ 
+@bp.route("/", methods=["GET", "POST"])
 def main():
-    return render_template("login.html")
+    if request.method == 'POST':
+        #subject_name = request.form['Subject Name']
+        #proctor_name = request.form['Proctor Name']
+        #db = get_db() 
+        #error = None
 
-@bp.route("/login",  methods=["GET", "POST"])
-def login():
-    return
+#code above might be useful later when we want to store user names and passwords 
+
+        #if error is None:
+        return redirect(url_for('landingpage.main'))
+    
+    return render_template('login.html')
+ 
+ 
+
 
 
--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 from breadth import bp as breadth_bp
 from depth import bp as depth_bp
 from login import bp as login_bp
+from landingpage import bp as landingpage_bp
 from flask import Flask
 
 
@@ -13,8 +14,14 @@ def create_app():
     app.register_blueprint(login_bp, url_prefix="/login")
     app.register_blueprint(breadth_bp, url_prefix="/breadth")
     app.register_blueprint(depth_bp, url_prefix="/depth")
+    app.register_blueprint(landingpage_bp, url_prefix="/landingpage")
     return app
 
 if __name__ == "__main__":
     app = create_app()
     app.run()
+
+#main is the first one called
+#if run.py is the intial file then run these two lines 
+#this is a include file
+#defines create app
