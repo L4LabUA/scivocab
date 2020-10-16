ffa814e42aed22192b7fe1685b1f28bbc94feda8
Siena Schoelen <schoelena@email.arizona.edu>
2020-10-16 16:32:35 -0700

    stored web form classes in forms.py

---
+++ forms.py
@@ -0,0 +1,8 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+
+class LoginForm(FlaskForm):
+    child_id = StringField('Child ID', validators=[DataRequired()])
+     proctor = StringField('Proctor', validators=[DataRequired()])
+    submit = SubmitField('Submit')
