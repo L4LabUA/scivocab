10ebbb1f869b286c9e41358a7d16ab2b241dae8c
Siena Schoelen <schoelena@email.arizona.edu>
2020-10-17 16:43:21 -0700

    work in progress to get functiona login

--- login.py
+++ login.py
@@ -1,21 +1,3 @@
-#original code is # out
-
-#from flask import Blueprint
-#from flask import render_template
-
-#bp = Blueprint("login", __name__)
-
-
-#@bp.route("/")
-#def main():
-#    return render_template("login.html")
-
-#@bp.route("/login",  methods=["GET", "POST"])
-#def login():
-#    return
-
-#the path: scivocab/login.py
- 
 import functools
  
 from flask import (
--- run.py
+++ run.py
@@ -3,7 +3,8 @@ from depth import bp as depth_bp
 from login import bp as login_bp
 from landingpage import bp as landingpage_bp
 from flask import Flask
-from flask_login import LoginManager
+#from flask_sqlalchemy import SQLAlchemy
+#from flask_login import LoginManager
 from config import Config 
 
 # Running run.py will launch the local flask server. It will run until manually shut down (or it crashes).
@@ -16,7 +17,7 @@ def create_app():
     app.register_blueprint(breadth_bp, url_prefix="/breadth")
     app.register_blueprint(depth_bp, url_prefix="/depth")
     app.register_blueprint(landingpage_bp, url_prefix="/landingpage")
-    login = LoginManager(app)
+    #login = LoginManager(app)
     app.config.from_object(Config)
     return app
 
