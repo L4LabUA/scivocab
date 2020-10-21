094afee389eb71231723e80415912ffa9d87e585
Siena Schoelen <schoelena@email.arizona.edu>
2020-10-21 13:45:28 -0700

    WIP - Trying to get the SECRET_KEY to work

--- app/config.py
+++ app/config.py
@@ -1,8 +1,8 @@
-import os
+#import os
 
-basedir = os.path.abspath(os.path.dirname(__file__))
+#basedir = os.path.abspath(os.path.dirname(__file__))
 
-class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you will never guess' # SECRET_KEY is used by Flask-wtf to protect webforms against CSRF. I think we will need to set the SECRET_KEY for the enviroment later?
-
+#class Config(object):
+#    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you will never guess' # SECRET_KEY is used by Flask-wtf to protect webforms against CSRF. I think we will need to set the SECRET_KEY for the enviroment later?
+#    app.config['SECRET_KEY'] = SECRET_KEY
 
--- app/main.py
+++ app/main.py
@@ -20,3 +20,6 @@ app = create_app()
 
 if __name__ == "__main__":
     app.run()
+
+
+
