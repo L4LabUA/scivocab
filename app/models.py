38276285881d800639fe0dcd93328671d72226dc
Siena Schoelen <schoelena@email.arizona.edu>
2020-11-04 12:43:35 -0700

    creating the database setup

---
+++ app/models.py
@@ -0,0 +1,10 @@
+from app import db
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    child_id  = db.Column(db.String(64), index=True, unique=True)
+    proctor = db.Column(db.String(64), index=True, unique=True)
+
+    def __repr__(self):
+        return '<User {}>'.format(self.child_id)   
+
