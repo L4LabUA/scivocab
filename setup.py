0dad619852d097ef4ab6ba8cc4d2440e24635570
Adarsh Pyarelal <adarsh@email.arizona.edu>
2020-11-18 17:16:01 -0700

    Adding setup.py to turn the app into a package with editable installs

---
+++ setup.py
@@ -0,0 +1,32 @@
+""" setuptools-based setup module. """
+
+from setuptools import setup, find_packages
+
+from setuptools import setup, Extension
+from setuptools.command.build_ext import build_ext
+
+setup(
+    name="app",
+    version="0.1",
+    description="Science Vocabulary Study webapp",
+    url="https://github.com/L4LabUA/scivocab",
+    packages = find_packages(),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Science/Research",
+    ],
+    keywords="speech analysis",
+    zip_safe=False,
+    install_requires=[
+        "flask",
+        "xlrd",
+        "numpy==1.18.0",
+        "pandas",
+        "openpyxl",
+        "Flask-WTF",
+        "Flask-SQLAlchemy",
+        "flask-migrate",
+    ],
+    python_requires=">=3.6",
+)
+
