c84912e9e2a7024ad3cfc0a01791478d16cbd0f7
Siena Schoelen <schoelena@email.arizona.edu>
2020-12-10 16:21:40 -0700

    Fixed models still working on login.py authentification

--- app/login.py
+++ app/login.py
@@ -19,7 +19,7 @@ bp = Blueprint("login", __name__)
 def main():
     #return render_template("landingpage.html")
     if current_user.is_authenticated:
-        return redirect(url_for('index'))
+        return render_template("landingpage.html")
     form = LoginForm()
                
     if form.validate_on_submit():
@@ -33,21 +33,23 @@ def main():
         # Check if the child is in the database - if they are not, then add
         # them. We might want to change this behavior later so that if they are
         # not in the database, we do not allow them to log in.
-        if Child.query.filter_by(id = child_id).first() is None:
-            child = Child(id = child_id)
+        if Child.query.filter_by(child_id = child_id).first() is None:
+            child = Child(child_id = child_id)
             db.session.add(child)
             db.session.commit()
 
         # Check if the proctor is in the database - if they are not, then add
         # them. We might want to change this behavior later so that if they are
         # not in the database, we do not allow them to log in.
-        if Proctor.query.filter_by(id = proctor_id).first() is None:
-            proctor = Proctor(id = proctor_id)
+        if Proctor.query.filter_by(proctor_id = proctor_id).first() is None:
+            proctor = Proctor(proctor_id = proctor_id)
             db.session.add(proctor)
             db.session.commit()
 
-        session = Session(id = str(uuid4()), child_id = child_id, proctor_id = proctor_id)
+        session = Session(session_id = str(uuid4()), child_id = child_id, proctor_id = proctor_id)
         db.session.add(session)
         db.session.commit()
+        child = Child.query.filter_by(child_id=child_id).first()
+        login_user(child, remember=form.remember_me.data)
         return render_template("landingpage.html")
     return render_template('login.html', title='Login', form=form)
--- app/models.py
+++ app/models.py
@@ -4,20 +4,23 @@ from flask_login import UserMixin
 from app import login_manager
 
 class Proctor(UserMixin, db.Model):
-    id = db.Column(db.String(64), primary_key = True, unique = True)
+    id = db.Column(db.Integer, primary_key=True)
+    proctor_id = db.Column(db.String(64), unique = True)
 
 class Child(UserMixin, db.Model):
-    id = db.Column(db.String(64), primary_key=True, unique=True)
+    id = db.Column(db.Integer, primary_key=True)
+    child_id = db.Column(db.String(64), unique = True)
 
 class Session(UserMixin, db.Model):
     # We give each session a UUID
-    id = db.Column(db.String, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
+    session_id = db.Column(db.String, unique=True)
     datetime = db.Column(db.DateTime, default = datetime.now())
     child_id = db.Column(db.String(64), db.ForeignKey("child.id"))
     proctor_id = db.Column(db.String(64), db.ForeignKey("proctor.id"))
 
 @login_manager.user_loader
-def load_user(id):
-    return Proctor.query.get(int(id))
+def load_user(child_id):
+    return Child.query.filter_by(child_id=child_id).first()
 
 
