bca9a71e6a9e974f5848274943efdd4b3118f2dd
Adarsh Pyarelal <adarsh@email.arizona.edu>
2020-12-22 15:28:51 -0700

    encapsulating logic in a new class called BreadthTaskManager

--- app/breadth.py
+++ app/breadth.py
@@ -9,6 +9,7 @@ from flask import (
     request,
     redirect,
     jsonify,
+    session,
 )
 import pandas as pd
 from dataclasses import dataclass, asdict
@@ -37,32 +38,45 @@ class Answer:
 # Flask blueprints help keep webapps modular.
 bp = Blueprint("breadth", __name__)
 
-# Imports all words from the given filename and stores them in a dictionary WORDS.
-WORDS = construct_word_dict(
-    Path(__file__).parents[0] / "static/scivocab/sv_bv1_input.csv"
-)
 
-# Splits the words in WORDS into four strands in STRANDS, where each strand
-# correlates to the strand types.
-STRANDS = [
-    [x for x in WORDS if WORDS[x].strand == n] for n in (40, 50, 62, 63)
-]
+class BreadthTaskManager(object):
+    def __init__(self):
+        # Imports all words from the given filename and stores them in a dictionary
+        self.words = construct_word_dict(
+            Path(__file__).parents[0] / "static/scivocab/sv_bv1_input.csv"
+        )
 
-for strand in STRANDS:
-    shuffle(strand)
+        # Splits the words in self.words into four strands in self.strands, where each strand
+        # correlates to the strand types.
+        strands = [
+            [x for x in self.words if self.words[x].strand == n]
+            for n in (40, 50, 62, 63)
+        ]
 
-# Shuffles the words in each strand, and concatenates each strand in a list
-# RANDOMIZED_LIST.  Then it creates a list where we store the user inputs
-# (which starts as an empty list, ANSWERS) and WORD_TYPES, which we will
-# use later.
-RANDOMIZED_LIST = list(chain(*STRANDS))
-ANSWERS = list()
+        for strand in strands:
+            shuffle(strand)
 
-# We use this in selectImage to put the images in a random order.
-WORD_TYPES = ["tw", "fp", "fx", "fs"]
+        # Shuffles the words in each strand, and concatenates each strand in a
+        # list self.randomized_list.  Then it creates a list where we store the
+        # user inputs (which starts as an empty list, self.answers) and
+        # self.word_types, which we will use later.
+        self.randomized_words = chain(*strands)
+        self.answers = []
 
-# Shuffle the WORD_TYPES list.
-shuffle(WORD_TYPES)
+        # We use this in selectImage to put the images in a random order.
+        self.word_types = ["tw", "fp", "fx", "fs"]
+
+        self.go_to_next_word()
+
+    def go_to_next_word(self):
+        # Shuffle the word_types list.
+        shuffle(self.word_types)
+
+        # Set the current word
+        self.current_word = next(self.randomized_words)
+
+
+manager = BreadthTaskManager()
 
 # If we are in debug mode, i.e., if the FLASK_DEBUG variable is set to 1, we
 # shorten the task to just a few words, for quick development and debugging
@@ -70,14 +84,13 @@ shuffle(WORD_TYPES)
 if os.environ.get("FLASK_DEBUG") == "1":
     N_WORDS_TO_SHOW = 3
 else:
-    N_WORDS_TO_SHOW = len(RANDOMIZED_LIST) - 1
+    N_WORDS_TO_SHOW = len(manager.randomized_list) - 1
 
 # Starts the app and leads to a loop of selectImage().
 @bp.route("/")
 @login_required
 def main():
-    current_word = RANDOMIZED_LIST[0]
-    return render_template("breadth.html", current_word=current_word)
+    return render_template("breadth.html", current_word=manager.current_word)
 
 
 @bp.route("/redirect")
@@ -86,28 +99,44 @@ def redirect_to_end():
 
 
 # Each call of selectImage loads a new word, waits for the user to select an
-# image, and adds the selected word to ANSWERS as an instance of the Answer
+# image, and adds the selected word to manager.answers as an instance of the Answer
 # class In doing so, it slowly iterates through the list RANDOMIZED_LIST, and
 # moves on to a new page when we reach the last word. In the process of ending,
-# it should call postprocessing(ANSWERS).
+# it should call postprocessing(manager.answers).
 @bp.route("/selectImage", methods=["GET", "POST"])
 def select_image():
     response_class = None
 
     if request.args.get("position"):
-        response_class = WORD_TYPES[int(request.args.get("position")[5]) - 1]
-        print(
-            "Selected response: "
-            + WORD_TYPES[int(request.args.get("position")[5]) - 1]
-        )
+        response_class = manager.word_types[
+            int(request.args.get("position")[5]) - 1
+        ]
 
     word_index = int(request.args.get("word_index", 0))
 
-    # If we've reached the last word, we stop and save the answers to Excel.
-    if word_index == N_WORDS_TO_SHOW:
+    try:
+        manager.go_to_next_word()
+        if response_class:
+            manager.answers.append(
+                Answer(
+                    manager.current_word,
+                    response_class,
+                    manager.words[manager.current_word].strand,
+                )
+            )
 
+        response = {
+            f"p{n}_filename": get_filename(
+                manager.words[manager.current_word], manager.word_types[n - 1]
+            )
+            for n in range(1, 5)
+        }
+        response["tw"] = manager.current_word
+
+        return jsonify(response)
+    except StopIteration:
         # Create a pandas DataFrame with the answer data.
-        df = pd.DataFrame([asdict(answer) for answer in ANSWERS])
+        df = pd.DataFrame([asdict(answer) for answer in manager.answers])
 
         # Save the dataframe as an Excel workbook with the filename
         # 'answers.xlsx'. This will overwrite any existing file with that name.
@@ -116,25 +145,3 @@ def select_image():
         # Since we use Ajax and jQuery, we cannot use the usual Flask redirect
         # function here. This is our workaround.
         return jsonify({"redirect": "redirect"})
-
-    # Otherwise, return a JSON object with data about the next word to show.
-    else:
-        current_word = RANDOMIZED_LIST[word_index]  # Here's the break
-        if response_class:
-            ANSWERS.append(
-                Answer(
-                    current_word, response_class, WORDS[current_word].strand
-                )
-            )
-
-        shuffle(WORD_TYPES)
-
-        response = {
-            f"p{n}_filename": get_filename(
-                WORDS[current_word], WORD_TYPES[n - 1]
-            )
-            for n in range(1, 5)
-        }
-        response["tw"] = current_word
-
-        return jsonify(response)
