3b993e09905f1a4a0be1881ed460ec501f9bfe72
Adarsh Pyarelal <adarsh@arizona.edu>
2021-04-20 16:35:40 -0700

    adding definition_task_responses.csv to gitignore, updating README, removing extra whitespace in config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you will never guess' # SECRET_KEY is used by Flask-wtf to protect webforms against CSRF. I think we will need to set the SECRET_KEY for the enviroment later?
-    
+
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
